5c021f88d0237a1499bf240ffba05006aca4006d
tish <latisha.griffiths@gmail.com>
2025-07-16 17:00:14 -0500

    uninstall @tailwindcss/vite and add release script for publing to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	base: '/rock-paper-scissors-game/', // Replace with your repo name
-	plugins: [react(), tailwindcss()],
+	plugins: [react()],
 	test: {
 		globals: true,
 		environment: 'jsdom',
